81580a1ac17acbb16577d41c4c6b49aab8911756
Vlastimil Elias <vlastimil.elias@worldonline.cz>
2013-09-23 08:57:10 +0200

    patched REST API initialization

--- src/main/java/org/jboss/elasticsearch/river/sysinfo/SysinfoRiverModule.java
+++ src/main/java/org/jboss/elasticsearch/river/sysinfo/SysinfoRiverModule.java
@@ -2,12 +2,6 @@ package org.jboss.elasticsearch.river.sysinfo;
 
 import org.elasticsearch.action.ActionModule;
 import org.elasticsearch.river.River;
-import org.jboss.elasticsearch.river.sysinfo.mgm.lifecycle.JRLifecycleAction;
-import org.jboss.elasticsearch.river.sysinfo.mgm.lifecycle.TransportJRLifecycleAction;
-import org.jboss.elasticsearch.river.sysinfo.mgm.period.JRPeriodAction;
-import org.jboss.elasticsearch.river.sysinfo.mgm.period.TransportJRPeriodAction;
-import org.jboss.elasticsearch.river.sysinfo.mgm.riverslist.ListRiversAction;
-import org.jboss.elasticsearch.river.sysinfo.mgm.riverslist.TransportListRiversAction;
 
 /**
  * System Info River ElasticSearch Module class.
@@ -16,15 +10,12 @@ import org.jboss.elasticsearch.river.sysinfo.mgm.riverslist.TransportListRiversA
  */
 public class SysinfoRiverModule extends ActionModule {
 
-  public SysinfoRiverModule() {
-    super(true);
-  }
+	public SysinfoRiverModule() {
+		super(true);
+	}
 
-  @Override
-  protected void configure() {
-    bind(River.class).to(SysinfoRiver.class).asEagerSingleton();
-    registerAction(ListRiversAction.INSTANCE, TransportListRiversAction.class);
-    registerAction(JRLifecycleAction.INSTANCE, TransportJRLifecycleAction.class);
-    registerAction(JRPeriodAction.INSTANCE, TransportJRPeriodAction.class);
-  }
+	@Override
+	protected void configure() {
+		bind(River.class).to(SysinfoRiver.class).asEagerSingleton();
+	}
 }
